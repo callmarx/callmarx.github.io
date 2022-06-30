8272aea5bca109b369dd3f219e3eaf093ede1135
callmarx <callmarx@users.noreply.github.com>
2022-06-30 00:42:55 +0000

    jekyll build from Action baf3dc3f741f4303bafab99e927902d80b48ce57

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,97 @@
+var store = [{
+        "title": "Meu primeiro post",
+        "excerpt":"Criei vergonha na cara e parei de ficar apenas babando nos inúmeros blogs sobre programação que encontramos pela imensidão da internet do gênero “What I Learned Today”. Vamos ver se consigo manter isso aqui, pelo menos para algo como “What I’ve Learned This Week”. “Tudo que temos de decidir é...","categories": ["blog"],
+        "tags": ["GitHubPages"],
+        "url": "/blog/2020/12/10/meu-primeiro-post.html",
+        "teaser": null
+      },{
+        "title": "Controller magro e model gordo, mas não perca o foco",
+        "excerpt":"Jamis Buck escreveu em 2006 o famoso post Skinny Controller, Fat Model, tornando-se quase que um mantra para seguir o padrão MVC - Model-View-Controller. No caso, toda lógica não relacionada à resposta ao usuário/cliente (View-Controller) deve entrar no model, mantendo assim a comunicação simples, ou melhor dizendo, “magra”. Por que...","categories": ["blog"],
+        "tags": ["DesignPatterns","Ruby","Rails"],
+        "url": "/blog/2020/12/17/controller-magro-model-gordo-n-perca-foco.html",
+        "teaser": null
+      },{
+        "title": "Busca em texto otimizada com a Gem pg_search - Parte I",
+        "excerpt":"Naufragando pela internet me deparei com a Gem PgSearch que tira proveito das funcionalidades de Full Text Searching do PostgreSQL. O conceito Full Text Searching refere a técnicas e estrateǵias computacionais para otimizar a pesquisa de palavras, e até frases, em longos e múltiplos textos armazenados em bancos de dados....","categories": ["blog"],
+        "tags": ["PostgreSQL","Ruby","Rails","Optimization","Config"],
+        "url": "/blog/2021/01/17/busca-texto-otimizada-com-pg-search-p1.html",
+        "teaser": null
+      },{
+        "title": "Busca em texto otimizada com a Gem pg_search - Parte II",
+        "excerpt":"Na parte I deste post eu expliquei um pouco sobre o conceito e as funcionalidades de Full Text Searching do PostgreSQL e me comprometi a explicar com um projetinho Ruby on Rails através da Gema PgSearch, vamos lá então. Git clone e diverta-se! O projeto completo está disponível em https://github.com/callmarx/fts_example....","categories": ["blog"],
+        "tags": ["PostgreSQL","Ruby","Rails","Optimization","Config"],
+        "url": "/blog/2021/04/08/busca-texto-otimizada-com-pg-search-p2.html",
+        "teaser": null
+      },{
+        "title": "Hoje eu aprendi: configurar verificador ortográfico PT-BR no editor Vim",
+        "excerpt":"Editando esse mesmo blog em meu vim (utilizo o Jekyll) me questionei: “Porque diabos estou usando o Google Docs como corretor?”. Decidi então buscar como configurar meu próprio vim - na verdade nvim, cheio plugins e firulas visuais - para fazer este trabalho e me poupar dos CTRL+C e CTRL+V....","categories": ["blog"],
+        "tags": ["Vim","Config","Hoje-eu-aprendi"],
+        "url": "/blog/2021/04/25/spellcheck_pt-br_no_vim.html",
+        "teaser": null
+      },{
+        "title": "Diário de um disléxico: Aprendendo Elixir",
+        "excerpt":"Como bom fanboy que sou de Ruby (e Rails), decidi estudar Elixir - linguagem brazuca e com sintaxe inspirada no Ruby. Cheguei a pensar em comprar algum curso online, mas como sou canguinha e mais implicante do que debugar java no Eclipse decidi seguir os tutoriais do Elixir School, o...","categories": ["blog"],
+        "tags": ["Elixir","Learn"],
+        "url": "/blog/2021/04/27/diario-de-um-dislexico_aprendendo-elixir.html",
+        "teaser": null
+      },{
+        "title": "Diário disléxico - Elixir: Um pouco mais que um \"Hello World\"",
+        "excerpt":"Para além de instalar e codar o clássico “Hello World”, vamos explorar os passos iniciais com a tecnologia. Entender os data types, as operações básicas e algumas curiosidades. Um pouco de história Elixir é uma linguagem de programação dinâmica, funcional e concorrente, compilada e executada na máquina virtual Erlang (BEAM...","categories": ["blog"],
+        "tags": ["Elixir","Learn"],
+        "url": "/blog/2021/05/06/diario-dislexico-elixir-mais-que-hello-world.html",
+        "teaser": null
+      },{
+        "title": "Diário disléxico - Elixir: preciso falar sobre \"Pattern Matching\"",
+        "excerpt":"Quanto mais eu pesquiso sobre Elixir em tutoriais, artigos, livros e vídeos pela internet, mais eu vejo isso sendo enfatizado. Inicialmente acreditei que fosse apenas por conta de legibilidade do código, mas provou-se bem mais do que isto, justificando a vasta cobertura que diversos autores dão. “Pattern Matching” Essa parte...","categories": ["blog"],
+        "tags": ["Elixir","Learn"],
+        "url": "/blog/2021/05/14/diario-dislexico-elixir-pattern-matching.html",
+        "teaser": null
+      },{
+        "title": "Diário disléxico - Elixir: Aquele clássico apanhadão",
+        "excerpt":"Antes de partir para a resolução de alguns exercícios, achei válido pontuar mais algumas funcionalidades do Elixir em um “apanhadão”. Neste post abordo sobre estruturas de controle, funções e operador Pipe, além de rever um pouco mais sobre pattern matching. Estruturas de Controle Obs: Me senti bem idiota depois de...","categories": ["blog"],
+        "tags": ["Elixir","Learn"],
+        "url": "/blog/2021/05/23/diario-dislexico-elixir-classico-apanhadao.html",
+        "teaser": null
+      },{
+        "title": "Diário disléxico - Elixir: Os primeiros exercícios",
+        "excerpt":"Volto aqui para falar um pouco sobre o que eu aprendi com os exercícios básicos e iniciantes do https://exercism.io sobre Elixir. Abaixo segue o que eu resolvi da trilha até agora e o que eu aprendi/utilizei em cada. Word Count: Pipe, String.split e regex RNA Transcription: map, Enum.map e codepoint...","categories": ["blog"],
+        "tags": ["Elixir","Learn"],
+        "url": "/blog/2021/08/29/diario-dislexico-elixir-primeiros-exercicios.html",
+        "teaser": null
+      },{
+        "title": "Hoje eu aprendi: Gerenciar dockers locais com Portainer CE",
+        "excerpt":"Mesmo não sendo um especialista, e tendo bem claro isso, ficou óbvio que definitivamente sou um entusiasta de contêineres. Hoje me deparei com a minha partição root do Linux quase cheia e por qual motivo? Inúmeras imagens, volumes e contêineres, que a maior parte eu nem estava mais utilizando, ocupando...","categories": ["blog"],
+        "tags": ["Hoje-eu-aprendi","Config","Docker"],
+        "url": "/blog/2021/09/03/dockers-e-portainer.html",
+        "teaser": null
+      },{
+        "title": "Tutorial: Rails7, Tailwind e Hotwire",
+        "excerpt":"Depois de meses desenvolvendo em Typescript com NestJS por demanda do meu atual trabalho, consegui um tempinho para meu tão amado Ruby on Rails. Melhor ainda agora, como não trabalho oficialmente com Rails, posso me dar o luxo de me aventurar na versão 7, recém lançada em modo alfa, em...","categories": ["blog"],
+        "tags": ["Tutorial","Rails","Ruby","Tailwind","Hotwire"],
+        "url": "/blog/2021/12/07/tutorial-rails7-hotwire.html",
+        "teaser": null
+      },{
+        "title": "Tutorial: Rails7, Tailwind e Hotwire - Parte 1",
+        "excerpt":"Na parte anterior deste tutorial, expliquei como configurar o Rails 7, com suas novas opções e como “dockerizei” os bancos de dados PostgreSQL e Redis. Agora vou abordar um pouco sobre Tailwind. Objetivo Geral A meta é desenvolver (e aprender) utilizando Rails 7, esbuild, Tailwind e Hotwire (Turbo e Stimulus),...","categories": ["blog"],
+        "tags": ["Tutorial","Rails","Ruby","Tailwind","Hotwire"],
+        "url": "/blog/2021/12/09/tutorial-rails7-hotwire-parte-1.html",
+        "teaser": null
+      },{
+        "title": "Tutorial: Rails7, Tailwind e Hotwire - Parte 2",
+        "excerpt":"Na parte anterior deste tutorial eu expliquei como customizar e utilizar o Tailwind sem uma linha sequer de CSS e JavaScript. Agora vou abordar um pouco sobre o pacote Hotwire Turbo. Objetivo Geral A meta é desenvolver (e aprender) utilizando Rails 7, esbuild, Tailwind e Hotwire (Turbo e Stimulus), mas...","categories": ["blog"],
+        "tags": ["Tutorial","Rails","Ruby","Tailwind","Hotwire"],
+        "url": "/blog/2021/12/19/tutorial-rails7-hotwire-parte-2.html",
+        "teaser": null
+      },{
+        "title": "Hoje eu aprendi: Vale a pena configurar seus dotfiles",
+        "excerpt":"Depois de mais de 3 anos usando Vim como IDE (Integrated Development Environment), migrando no caminho para o Neovim com o shell zsh, o gerenciador Oh My Zsh e o tema Powerlevel10k, percebi como eu dependia dessas configurações e como as mesmas estavam totalmente desorganizadas. Resolvi então, seguir os passos...","categories": ["blog"],
+        "tags": ["Hoje-eu-aprendi","Config","Vim","Neovim","dotfiles","Linux"],
+        "url": "/blog/2022/06/08/dotfiles.html",
+        "teaser": null
+      },{
+        "title": "Tutorial: Rails7, Tailwind e Hotwire - Parte 3",
+        "excerpt":"Na parte anterior deste tutorial eu expliquei como utilizar a renderização parcial de html com turbo_stream do Hotwire Turbo, o que nos permitiu mostrar os cards recém inseridos ou excluídos do nosso humilde protótipo de Kanban. Agora vou abordar sobre o pacote Hotwire Stimulus. Objetivo Geral A meta é desenvolver...","categories": ["blog"],
+        "tags": ["Tutorial","Rails","Ruby","Tailwind","Hotwire"],
+        "url": "/blog/2022/06/29/tutorial-rails7-hotwire-parte-3.html",
+        "teaser": null
+      }]
